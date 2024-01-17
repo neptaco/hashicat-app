5a7dfb46bfe4279b3abc31ff1086e11618cd4027
neptaco <746984+neptaco@users.noreply.github.com>
2024-01-17 06:45:16 +0000

    Updated HTML payload

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -13,7 +13,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to ${PREFIX}'s app. GITHUB_ORGANIZATIONの設定が抜けていますよ.
   <!-- END -->
 
   </div>
